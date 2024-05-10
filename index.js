0d49ff1e37960e926a814161821040bafc26c8c5
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-05-11 01:45:41 +0600

    users comment save to the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -26,6 +26,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const blogs = client.db("storyForge").collection("blogs");
+    const comments = client.db("storyForge").collection("comments");
+
+// getting blog data from database collection
+app.get("/blogs", async (req,res)=>{
+  const cursor = blogs.find();
+  const result = await cursor.toArray();
+  res.send(result)
+})
+// get single data for blog details
+app.get("/all-blogs/:id", async(req, res)=>{
+const id = req.params.id
+const query = {_id: new ObjectId(id)}
+const result = await blogs.findOne(query)
+res.send(result)
+})
+
+
+
+
+// save comment to the database
+app.post('/comments', async (req, res)=>{
+  const comment = req.body;
+  const result = await comments.insertOne(comment)
+  res.send(result)
+})
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
